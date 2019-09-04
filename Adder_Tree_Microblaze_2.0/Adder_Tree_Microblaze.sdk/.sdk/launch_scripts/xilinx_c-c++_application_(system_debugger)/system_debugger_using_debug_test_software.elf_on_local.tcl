27b75c8c2b174172a3448f333b8f34b3547d1da2
jorge.portilla <jorge.portilla@sansano.usm.cl>
2019-09-03 23:45:54 -0400

    eliminate

--- Adder_Tree_Microblaze_2.0/Adder_Tree_Microblaze.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_test_software.elf_on_local.tcl
+++
@@ -1,8 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-configparams mdm-detect-bscan-mask 2
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A3FD96A"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A3FD96A"} -index 0
-dow /home/jorge/VivadoUSM/Designing-a-Custom-AXI-Slave-Peripheral-master/axi_rc_servo_controller_v2_00_a/axi_rc_servo_controller_v2_00_a.sdk/test_software/Debug/test_software.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A3FD96A"} -index 0
-con
